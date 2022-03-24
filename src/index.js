f1f82effb60eed903de5d6cd00b0973da90b6708
Pavel Trushnikov <oracal@mail.ru>
2022-03-24 21:34:35 +0300

    Реализация блока Feature

--- src/index.js
+++ src/index.js
@@ -1,7 +1,8 @@
 import {Tabs} from '@module/tabs';
+import {Feature} from '@module/feature';
 
 
 // Tabs
 new Tabs('.design-wrapper');
 // Feature
-
+new Feature('.feature-wrapper');
--- src/module/feature.js
+++ src/module/feature.js
@@ -1,1 +1,29 @@
-ommit
+import {Domlistener} from '@module/domlistener';
+
+export class Feature extends Domlistener {
+    constructor(el) {
+        super(el);
+        this.changeElem(this);
+    }
+
+    changeElem(el) {
+        this.addListener(this.el, 'click');
+    }
+
+    getElem(event) {
+        if (this.$target.closest('.feature-item-wrap')) {
+            const btnFeature = this.el.querySelectorAll('button');
+            const featureSub = this.el.querySelectorAll('.feature-sub');
+            btnFeature.forEach( (btn) => {
+                btn.classList.remove('feature__link_active');
+            });
+            featureSub.forEach( (desc) => {
+                desc.classList.add('hidden');
+            });
+            const btn = this.$target.closest('.feature__link');
+            btn.classList.add('feature__link_active');
+            btn.nextElementSibling.classList.remove('hidden');
+        }
+        console.log(this);
+    }
+}
